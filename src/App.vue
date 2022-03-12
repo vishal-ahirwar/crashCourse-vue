d8ab7ecd1e3267fa51fb85247f8a6fc5bd915ec2
IVishalAhirwar <vishalchoudhry0091@gmail.com>
2022-03-13 00:49:11 +0530

    updated home component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <home-component CopyrightNotice="Copyright(c) 2022 Vishal Ahirwar. All rights reserved." />
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HomeComponent from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    HomeComponent
   }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,66 @@
 <template>
+<div>
+    <button v-on:click="display">Toggle Show</button>
+    <button v-if="show" v-on:click="helloAlert">hello Alert is Active</button>
+    <button v-else>hello</button>
+    <table border="2px">
+    <tr>
+        <td>ID</td>
+        <td>User Name</td>
+        <td>Email Address</td>
+        </tr>
+        <tr v-for="user in users" :key="user.id">
+            <td>{{user.id}}</td>
+            <td>{{user.name}}</td>
+            <td>{{user.mail}}</td>
+    </tr>
+    </table>
     <h1>Hello From Home component</h1>
+    <p>{{CopyrightNotice}}</p>
+</div>
 </template>
+
 <script>
+
+
     export default{
-        name:"Home"
+        name:"HomeComponent",
+        props:{
+            CopyrightNotice:String,
+        },
+        
+            helloAlert()
+            {
+                alert("Hello, There How can I help you??");
+            },
+            data()
+            {
+        
+                return {show:false}
+            },
+
+
+            methods:
+            {
+                displayUsersInfo()
+                {
+                    if(this.show)
+                    {
+                        return {users:[{id:1,name:"Vishal Ahirwar",mail:"thisismail@vmail.com"},
+                                        {id:2,name:"Cishal Ahirwar",mail:"thisiCmail@vmail.com"},
+                                        {id:3,name:"Hishal Ahirwar",mail:"thiHsismail@vmail.com"}
+                                        ]
+                                }
+                    }else
+                    {
+                        return{users:[]}
+                    }
+                }
+,
+                display()
+                {
+                    this.show=!this.show
+                },
+            }
     }
 </script>
